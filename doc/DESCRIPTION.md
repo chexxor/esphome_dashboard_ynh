68cdcf60e7699a5f27bf0c784b97df80701df543
Alex Berg <chexxor@gmail.com>
2022-12-17 15:17:03 -0600

    Initial commit

--- doc/DESCRIPTION.md
+++ doc/DESCRIPTION.md
@@ -1,9 +1,7 @@
-Some long and extensive description of what the app is and does, lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+The ESPHome Dashboard is a user facing dashboard embedded in ESPHome. It allows users to easily create and manage their configurations.
+
+ESPHome is a system to control your ESP8266/ESP32 by simple yet powerful configuration files and control them remotely through Home Automation systems
 
 ### Features
 
-- Ut enim ad minim veniam, quis nostrud exercitation ullamco ;
-- Laboris nisi ut aliquip ex ea commodo consequat ;
-- Duis aute irure dolor in reprehenderit in voluptate ;
-- Velit esse cillum dolore eu fugiat nulla pariatur ;
-- Excepteur sint occaecat cupidatat non proident, sunt in culpa."
+- ...
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -18,14 +18,71 @@
 # PHP dependencies used by the app (must be on a single line)
 #php_dependencies="php$YNH_PHP_VERSION-deb1 php$YNH_PHP_VERSION-deb2"
 # or, if you do not need a custom YNH_PHP_VERSION:
-php_dependencies="php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+# php_dependencies="php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="deb1 deb2 $php_dependencies"
+pkg_dependencies="python3 python3-dev python3-venv python3-pip"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+
+# Install/Upgrade ESPHome in virtual environement
+myynh_install_esphome () {
+	# Create the virtual environment
+	ynh_exec_as $app $py_app_version -m venv --without-pip "$final_path"
+
+	# Run source in a 'sub shell'
+	(
+		# activate the virtual environment
+		set +o nounset
+		source "$final_path/bin/activate"
+		set -o nounset
+
+		# add pip
+		ynh_exec_as $app "$final_path/bin/python3" -m ensurepip
+
+		# install last version of wheel
+		# ynh_exec_as $app "$final_path/bin/pip3" --cache-dir "$data_path/.cache" install --upgrade wheel
+
+		# install ESPHome
+		ynh_exec_as $app "$final_path/bin/pip3" --cache-dir "$data_path/.cache" install --upgrade tornado esphome
+	)
+}
+
+# Install/Upgrade ESPHome Dashboard in virtual environement
+myynh_install_esphomedashboard () {
+	# Create the virtual environment
+	ynh_exec_as $app $py_app_version -m venv --without-pip "$final_path"
+
+	# Run source in a 'sub shell'
+	(
+		# activate the virtual environment
+		set +o nounset
+		source "$final_path/bin/activate"
+		set -o nounset
+
+		# add pip
+		ynh_exec_as $app "$final_path/bin/python3" -m ensurepip
+
+		# install last version of wheel
+		# ynh_exec_as $app "$final_path/bin/pip3" --cache-dir "$data_path/.cache" install --upgrade wheel
+
+		# install ESPHome
+		ynh_exec_as $app "$final_path/script/build"
+	)
+}
+
+# Upgrade the virtual environment directory
+myynh_upgrade_venv_directory () {
+	ynh_exec_as $app $py_app_version -m venv --upgrade "$final_path"
+}
+
+# Create directory only if not already exists (path in argument)
+myynh_create_dir () {
+	[ -z "$1" ] && ynh_die "No argument supplied"
+	[ -d "$1" ] || mkdir -p "$1"
+}
 
 #=================================================
 # EXPERIMENTAL HELPERS
